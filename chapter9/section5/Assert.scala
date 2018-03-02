8e769d2fa3507aacb60a90800a03844187a07c86
Zobody <16544117+Zobody@users.noreply.github.com>
2018-03-02 23:19:12 +0200

    Added a Listing

--- chapter9/section5/Assert.scala
+++ chapter9/section5/Assert.scala
@@ -6,3 +6,11 @@ def myAssert(predicate: () => Boolean) =
 		throw new AssertionError
 
 myAssert(() => 5 > 3) // This is weird
+
+// Listing 9.5 : Using a by-name parameter.
+
+def byNameAssert(predicate: => Boolean) = 
+	if (assertationsEnabled && !predicate())
+		throw new AssertionError
+
+byNameAssert(5 > 3)
