40038eb50882936dfab147174f847ac1b5676bb3
Zobody <16544117+Zobody@users.noreply.github.com>
2018-03-02 23:24:51 +0200

    Added a way to seemingly simplify code...

--- chapter9/section5/Assert.scala
+++ chapter9/section5/Assert.scala
@@ -13,4 +13,12 @@ def byNameAssert(predicate: => Boolean) =
 	if (assertationsEnabled && !predicate())
 		throw new AssertionError
 
-byNameAssert(5 > 3)
+byNameAssert(5 > 3) // Ah, a lot better
+
+// Hold up tho...
+
+def boolAssert(predicate: Boolean) = 
+	if (assertationsEnabled && !predicate) // A little bit different, the `!predicate vs !predicate()` part
+		throw new AssertionError
+
+boolAssert(5 > 3) // Pretty much the same
