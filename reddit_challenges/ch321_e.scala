acf8fff73f28ef4102c5c17a234445c0ca069d4c
Zobody <16544117+Zobody@users.noreply.github.com>
2018-03-10 12:40:21 +0200

    Added a convert from Strings to Ints

--- reddit_challenges/ch321_e.scala
+++ reddit_challenges/ch321_e.scala
@@ -1,10 +1,18 @@
 
+
 def timeToText(input: String) : String = {
+
 	val hourPart: String = input.substring(0,2)
 	val minutesPart: String = input.substring(3,5)
 
-	println(hourPart + " | " + minutesPart)
+	val hoursInt: Int = hourPart.toInt
+	val minutesTens: Int = minutesPart.charAt(0).toInt - '0'.toInt
+	val minutesOnes: Int = minutesPart.charAt(1).toInt - '0'.toInt
 
+
+	// temp
+	println(hourPart + " | " + minutesPart) 
+	println(minutesTens + " | " + minutesOnes)
 	"WARNING"
 }
 
