c9e4885c070f0ddf478392f403a73f54a9132c92
Zobody <16544117+Zobody@users.noreply.github.com>
2018-03-24 11:07:46 +0200

    Revert "Renamed some files."
    This reverts commit 4b429f98fc4b00b28cea39e139c7eb15320077ee.
    
    Turns out I didn't rename one file, I outright deleted it.

--- chapter10/section3/Element.scala
+++
@@ -1,7 +0,0 @@
-// Listing 10.2 : Defining parameterless methods width and height.
-
-abstract class Element {
-	def contents: Array[String]
-	def height: Int = contents.length
-	def width: Int = if (height == 0) 0 else contents(0).length
-}
