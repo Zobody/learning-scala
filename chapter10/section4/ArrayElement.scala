2d59d2706a6b6e5ba6a08f6664f6a4d36204a630
Zobody <16544117+Zobody@users.noreply.github.com>
2018-03-24 11:15:45 +0200

    Fixed a mistake where I forgot to write what Array was equal to (it should have been the parameter `conts`)

--- chapter10/section4/ArrayElement.scala
+++ chapter10/section4/ArrayElement.scala
@@ -1,5 +1,5 @@
 // Listing 10.3 : Defining ArrayElement as a subclass of Element.
 
 class ArrayElement(conts: Array[String]) extends Element {
-	def contents: Array[String] =
+	def contents: Array[String] = conts
 }
