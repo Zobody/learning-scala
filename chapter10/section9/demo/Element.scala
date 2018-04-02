da3dd10b2f248849d1d05b3b9aa7c642f847790b
Zobody <16544117+Zobody@users.noreply.github.com>
2018-04-02 18:47:29 +0300

    Removed all existing members from Element class and added a method named demo to Element.

--- chapter10/section9/demo/Element.scala
+++ chapter10/section9/demo/Element.scala
@@ -1,7 +1,7 @@
 // Listing 10.2 : Defining parameterless methods width and height.
 
 abstract class Element {
-	def contents: Array[String]
-	def height: Int = contents.length
-	def width: Int = if (height == 0) 0 else contents(0).length
+	def demo() {
+		println("Element's implementation invoked")
+	}
 }
