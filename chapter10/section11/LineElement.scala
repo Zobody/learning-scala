b3dac6ac3fd108b8e463b84f12a2e480ea8d82a0
Zobody <16544117+Zobody@users.noreply.github.com>
2018-04-12 17:06:37 +0300

    Made it so LineElement inherits from Element

--- chapter10/section11/LineElement.scala
+++ chapter10/section11/LineElement.scala
@@ -1,6 +1,7 @@
-// Listing 10.6 : Invoking a superclass constructor
 
-class LineElement(s: String) extends ArrayElement(Array(s)) {
+
+class LineElement(s: String) extends Element {
+	val contents = Array(s)
 	override def width = s.length
 	override def height = 1
 }
