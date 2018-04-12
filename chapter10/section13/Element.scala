ef5071c6c6af6fbf2ef4872708878676f13c01aa
Zobody <16544117+Zobody@users.noreply.github.com>
2018-04-12 17:40:38 +0300

    Copying over files to new Section

---
+++ chapter10/section13/Element.scala
@@ -0,0 +1,29 @@
+// Listing 10.9 : Class Element with above, beside, and toString.
+
+abstract class Element {
+	def contents: Array[String]
+	def height: Int = contents.length
+	def width: Int = if (height == 0) 0 else contents(0).length
+
+	def above(that: Element): Element = 
+		new ArrayElement(this.contents ++ that.contents)
+	/*
+	def beside(that: Element): Element = {
+		val contents = new Array[String](this.contents.length)
+		for (i <- 0 until this.contents.length)
+			contents(i) = this.contents(i) + that.contents(i)
+		new ArrayElement(contents)
+	}
+	*/
+
+	def beside(that: Element): Element = {
+		new ArrayElement(
+			for (
+				(line1, line2) <- this.contents zip that.contents
+			) yield {line1 + line2}
+		)
+	}
+
+	override def toString = contents mkString "\n"
+
+}
