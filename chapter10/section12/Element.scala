df270563f775a99db0d2bd30a93489c1dcd3af2e
Zobody <16544117+Zobody@users.noreply.github.com>
2018-04-12 17:14:56 +0300

    Added an implementation of above function

--- chapter10/section12/Element.scala
+++ chapter10/section12/Element.scala
@@ -1,7 +1,10 @@
-// Listing 10.2 : Defining parameterless methods width and height.
+
 
 abstract class Element {
 	def contents: Array[String]
 	def height: Int = contents.length
 	def width: Int = if (height == 0) 0 else contents(0).length
+
+	def above(that: Element): Element = 
+		new ArrayElement(this.contents ++ that.contents)
 }
