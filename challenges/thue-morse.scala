9551560f69be51fd53d6f85bcb3f72ef6e5d5f91
Zobody <16544117+Zobody@users.noreply.github.com>
2018-04-13 18:34:39 +0300

    Converts numbers to letters and back

--- challenges/thue-morse.scala
+++ challenges/thue-morse.scala
@@ -8,18 +8,31 @@
 object ThueMorse {
 
 	def main(input: Array[String]) {
-		val string = compute(
-			input(0),
-			input(1).toInt
+		val numbers = input(0)(0) == '0' || input(0)(0) == '1'
+		val times = if (input.length == 1) 1 else input(1).toInt
+
+		val mappingToLetters = Map('0' -> 'A', '1' -> 'B', 'A' -> 'A', 'B' -> 'B')
+		val mappingToNumbers = Map('A' -> '0', 'B' -> '1')
+
+		val processedInput = process(input(0) map (_.toUpper), mappingToLetters)
+
+		val output = compute(
+			processedInput,
+			times
 		)
-		println(string)
+
+		val processedOutput = if (numbers) process(output, mappingToNumbers) else output
+
+		println(processedOutput)
 	}
 
-	def invert(input: String) : String = input map (s => {if (s == 'A') 'B' else 'A'})
+	def invert(input: String) : String = input map (c => {if (c == 'A') 'B' else 'A'})
 
 	def compute(input: String, times: Int): String = {
 		if (times == 0) input
 		else compute(input + invert(input), times - 1)
 	}
+
+	def process(input: String, mapping: Map[Char, Char]) : String = input map (c => {mapping(c)})
 
 }
