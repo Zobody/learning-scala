50171fe972658f091f1d6c0be9ff8ecf6be5540c
Zobody <16544117+Zobody@users.noreply.github.com>
2018-04-13 17:15:33 +0300

    Starting my own challenge, with a basic start and no functionality

---
+++ challenges/thue-morse.scala
@@ -0,0 +1,12 @@
+
+/*
+	This is my self defined challenge.
+
+	This program will take in input with either 0s and 1s, or As and Bs, and return input + !input
+*/
+
+object ThueMorse {
+	def main(val input: Array[String]) {
+		for (s <- input) {println(s)}
+	}
+}
