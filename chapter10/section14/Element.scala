2f364ed490a7d7241248794b0fa3740632196f7f
Zobody <16544117+Zobody@users.noreply.github.com>
2018-07-09 18:06:21 +0300

    Added widen and heighten functions

--- chapter10/section14/Element.scala
+++ chapter10/section14/Element.scala
@@ -26,6 +26,22 @@ abstract class Element {
 		)
 	}
 
+	def widen(w: Int): Element = 
+		if (w <= width) this
+		else {
+			val left = elem(' ', (w - width) / 2, height)
+			val right = elem(' ', w - width - left.width, height)
+			left beside this beside right
+		}
+
+	def heighten(h: Int): Element = 
+		if (h <= height) this
+		else {
+			val top = elem(' ', width, (h - height) / 2)
+			val bottom = elem(' ', width, h - height - top.height)
+			top above this above bottom
+		}
+
 	override def toString = contents mkString "\n"
 
 }
