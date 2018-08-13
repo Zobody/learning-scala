e0185638c9a023c38a199462ec11c519038ecbdf
Zobody <16544117+Zobody@users.noreply.github.com>
2018-08-13 15:30:21 +0300

    Moving things around, fixing compile errors

--- challenges/game.scala
+++ challenges/game.scala
@@ -3,21 +3,75 @@ import scala.collection.mutable.ArrayBuffer
 
 object GameTest extends App {
 
+	abstract class Creature(val health: Int, val offense: Int, val defense: Int) {
+
+		def receiveDamage: Int => Creature
+		def directDamage: Int => Creature
+
+		def attack: Creature => Creature
+
+		def isDead: Boolean = health <= 0
+	}
+
+	class Player(val health: Int, val offense: Int, val defense: Int) extends Creature(health, offense, defense) {
+		def attack(that: Creature): Creature = {
+			that.receiveDamage(this.offense)
+		}
+
+		def receiveDamage(damage: Int): Player = {
+			val newDefense = if (defense > damage) defense - damage else 0
+			val newHealth = if (damage > defense) health - (damage - defense) else health
+
+			new Player(newHealth, offense, newDefense)
+		}
+
+		def directDamage(damage: Int): Player = {
+			val newHealth = health - damage
+
+			new Player(newHealth, offense, defense)
+		}
+	}
+
+	abstract class Monster(val health: Int, val offense: Int, val defense: Int, val name: String) extends Creature(health, offense, defense)
+
+	class RegularMonster(val health: Int, val offense: Int, val defense: Int, val name: String) extends Monster(health, offense, defense, name) {
+		def attack(that: Creature): Creature = {
+			that.receiveDamage(this.offense)
+		}
+
+		def receiveDamage(damage: Int): RegularMonster = {
+			val newDefense = if (defense > damage) defense - damage else 0
+			val newHealth = if (damage > defense) health - (damage - defense) else health
+
+			new RegularMonster(newHealth, offense, newDefense, name)
+		}
+
+		def directDamage(damage: Int): RegularMonster = {
+			val newHealth = health - damage
+
+			new RegularMonster(newHealth, offense, defense, name)
+		}
+	}
+
 	class Universe(val field: List[Creature], state: String) {
 
 		def update(input: String): Universe = {
 			this
 		}
 
-		def output(outputUniverse: Universe): Unit = {
+		def output(): Unit = {
 
-			if (outputUniverse.state == "REGULAR") {
+			if (state == "REGULAR") {
 				println("-"*15)
 				println()
 				println("On the field, currently there are: ")
 
 				for (creature <- field) {
-					val name = if (creature.isInstanceOf[Player]) "Player" else creature.name
+					val name = creature match {
+						case m: Monster => creature.name
+						case p: Player => "Player"
+						case _ => "BAD_DATA"
+					}
 					val index = field.indexOf(creature)
 					println("[" + index + "] "  name + "with the following stats: HP: " + creature.health + ", ATK: " + creature.attack + ", DEF: " + creature.defense)
 				}
@@ -26,7 +80,7 @@ object GameTest extends App {
 				println("-"*15)
 				println("You can do one of these things: attack <N>, rest")
 				print("> ")
-			} else if (outputUniverse.state == "BATTLE") {
+			} else if (state == "BATTLE") {
 				println("-"*15)
 				println()
 				println("You deal " + field.head.attack + " damage to ")
@@ -36,55 +90,7 @@ object GameTest extends App {
 
 		}
 
-		abstract class Creature(val health: Int, val attack: Int, val defense: Int) {
-
-			def receiveDamage: Int => Creature
-			def directDamage: Int => Creature
-
-			def attack: Creature => Creature
-
-			def isDead: Boolean = health <= 0
-		}
-
-		class Player(val health: Int, val attack: Int, val defense: Int) extends Creature(health, attack, defense) {
-			def attack(that: Creature): Creature = {
-				that.receiveDamage(this.attack)
-			}
-
-			def receiveDamage(damage: Int): Player = {
-				val newDefense = if (defense > damage) defense - damage else 0
-				val newHealth = if (damage > defense) health - (damage - defense) else health
-
-				Player(newHealth, attack, newDefense)
-			}
-
-			def directDamage(damage: Int): Player = {
-				val newHealth = health - damage
-
-				Player(newHealth, attack, defense)
-			}
-		}
-
-		abstract class Monster(val health: Int, val attack: Int, val defense: Int, val name: String) extends Creature(health, attack, defense) {
-			def receiveDamage(damage: Int): Monster = {
-				val newDefense = if (defense > damage) defense - damage else 0
-				val newHealth = if (damage > defense) health - (damage - defense) else health
-
-				Monster(newHealth, attack, newDefense, name)
-			}
-
-			def directDamage(damage: Int): Monster = {
-				val newHealth = health - damage
-
-				Monster(newHealth, attack, defense, name)
-			}
-		}
-
-		class RegularMonster(val health: Int, val attack: Int, val defense: Int, val name: String) extends Monster(health, attack, defense, name) {
-			def attack(that: Creature): Creature = {
-				that.receiveDamage(this.attack)
-			}
-		}
+		
 	}
 
 	def init(): Universe = {
@@ -101,7 +107,7 @@ object GameTest extends App {
 	def mainLoop(inputUniverse: Universe): Universe = {
 		val userInput = receiveInput()
 		val outputUniverse = inputUniverse.update(userInput)
-		outputUniverse.output
+		outputUniverse.output()
 		outputUniverse
 	}
 
