e34560ff73712da5ef9b8232c58f0862919307a4
Zobody <16544117+Zobody@users.noreply.github.com>
2018-11-27 20:53:47 +0200

    Rest of the example, and fixed a typo somewhere

--- chapter12/rectangle.scala
+++ chapter12/rectangle.scala
@@ -5,7 +5,15 @@ trait Rectangular {
 	def bottomRight: Point
 
 	def left = topLeft.x
-	def right = bottomright.x
+	def right = bottomRight.x
 	def width = right - left
 	// etc
+}
+
+abstract class Component extends Rectangular {
+	// somethign about Component here, probably
+}
+
+class Rectangle(val topLeft: Point, val bottomRight: Point) extends Rectangular {
+	// again, whatever Rectangles need.
 }
