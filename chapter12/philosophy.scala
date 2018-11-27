feb05ad57ab434ecaef14aeadcf2c657e7583fc1
Zobody <16544117+Zobody@users.noreply.github.com>
2018-11-27 20:39:42 +0200

    Listing 12.3 and 12.4 : Mixing in traits using with

--- chapter12/philosophy.scala
+++ chapter12/philosophy.scala
@@ -4,6 +4,9 @@ trait Philosophical {
 	}
 }
 
-class Frog extends Philosophical {
+class Animal
+trait HasLegs
+
+class Frog extends Animal with Philosophical with HasLegs {
 	override def toString = "green"
 }
