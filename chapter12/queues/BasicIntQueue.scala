21f5258f52e004236a3bdf5ae305eb4b0c8c9e04
Zobody <16544117+Zobody@users.noreply.github.com>
2018-11-29 19:10:40 +0200

    Added multiple mixins

--- chapter12/queues/BasicIntQueue.scala
+++ chapter12/queues/BasicIntQueue.scala
@@ -4,6 +4,16 @@ trait Doubling extends IntQueue {
 	abstract override def put(x: Int) { super.put(2*x) }
 }
 
+trait Incrementing extends IntQueue {
+	abstract override def put(x: Int) { super.put(x+1) }
+}
+
+trait Filtering extends IntQueue {
+	abstract override def put(x: Int) {
+		if (x >= 0) super.put(x)
+	}
+}
+
 class BasicIntQueue extends IntQueue {
 	private val buf = new ArrayBuffer[Int]
 	def get() = buf.remove(0)
