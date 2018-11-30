8cfbc0771963f54ad03534cb36a3c3c0c7ddc1ee
Zobody <16544117+Zobody@users.noreply.github.com>
2018-11-30 14:28:12 +0200

    Did something with package objects To run this code, first compile it with `scalac *.scala` in the fruits folder, and then you can run with with `scala printmenu.PrintMenu`

--- chapter13/fruits/BobsDelights.scala
+++ chapter13/fruits/BobsDelights.scala
@@ -1,26 +1,34 @@
-package bobsdelights
+package bobsdelights {
 
-abstract class Fruit(
-	val name: String,
-	val color: String
-)
+	abstract class Fruit(
+		val name: String,
+		val color: String
+	)
 
-object Fruits {
-	object Apple extends Fruit("apple", "red") with Showable
-	object Orange extends Fruit("orange", "orange") with Showable
-	object Pear extends Fruit("pear", "green") with Showable
-	val manu = List(Apple, Orange, Pear)
+	object Fruits {
+		object Apple extends Fruit("apple", "red")
+		object Orange extends Fruit("orange", "orange")
+		object Pear extends Fruit("pear", "green")
+		val menu = List(Apple, Orange, Pear)
+	}
+
 }
 
-trait Showable { // this is THE BIGGEST MESS POSSIBLE
+package object bobsdelights {
 	def showFruit(fruit: Fruit) {
 		import fruit._
-		println(name + "s are " + color)
+		println(name + "s are " + color + " and very delicious")
 	}
 }
 
-import java.util.regex // package names can also be imported, so instead of using java.util.regex each time, regex may be used
 
-class AStarB {
-	val pat = regex.Pattern.compile("a*b")
+
+package somethingAboutRegex {
+
+	import java.util.regex // package names can also be imported, so instead of using java.util.regex each time, regex may be used
+
+	class AStarB {
+		val pat = regex.Pattern.compile("a*b")
+	}
+
 }
